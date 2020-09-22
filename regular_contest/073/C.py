3160286d90474cfeb69806cebd306426972fa728
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-09-22 14:38:45 +0900

    ARC 073 C - Sentou

---
+++ regular_contest/073/C.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 7)
+
+n, T = map(int, input().split())
+t = list(map(int, input().split()))
+ans = 0
+for i in range(n - 1):
+    ans += min(T, t[i+1] - t[i])
+ans += T
+print(ans)
