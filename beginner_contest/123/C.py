19ac9e9bb63e6bd8001a2bf17b0efb55fbc69c2e
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-10-09 02:04:39 +0900

    ABC 123 C - Five Transportations

---
+++ beginner_contest/123/C.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 7)
+
+n = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+print(((n + min(a,b,c,d,e) - 1) // min(a,b,c,d,e)) + 4)
